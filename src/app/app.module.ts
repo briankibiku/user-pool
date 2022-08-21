6651132f5771bddfb47e64b2f6a2b4c7109c6551
kibiku <kibikukim@gmail.com>
2022-08-21 14:18:18 +0300

    feat: add user page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { UsersComponent } from './users/users.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import {
+  IgxAvatarModule,
+  IgxFilterModule,
+  IgxIconModule,
+  IgxListModule,
+  IgxInputGroupModule,
+  IgxButtonGroupModule,
+  IgxRippleModule,
+} from 'igniteui-angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserComponent,
-    UsersComponent,
-  ],
+  declarations: [AppComponent, UsersComponent, UserListComponent],
   imports: [
+    MatDialogModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    IgxListModule,
+    BrowserAnimationsModule,
+    IgxAvatarModule,
+    IgxFilterModule,
+    IgxIconModule,
+    IgxListModule,
+    IgxInputGroupModule,
+    IgxButtonGroupModule,
+    IgxRippleModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
